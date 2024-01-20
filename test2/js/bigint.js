b30875a5c6c0549455f6ee93b166f121b0f0488d
lucas2003a <lucasatuncar1@gmail.com>
2024-01-20 16:13:00 -0500

    Big int - Métodos

--- test2/js/bigint.js
+++ test2/js/bigint.js
@@ -14,24 +14,55 @@ document.addEventListener("DOMContentLoaded",()=>{
             console.log("");
 
     //-------------------------------------------------------------------------------------
-    const number = 2**53;
+    //Diferencias de presición entre un nmber y un int.
+        //Ejemplo 1 - number:
+                const number = 2**53;
 
-    //Averiguamos el tipo de dato
-    console.log("Tipo de dato de number: ",number.constructor.name)         //Number    =>  Es un tipo de dato number.
+                //Averiguamos el tipo de dato.
+                console.log("Tipo de dato de number: ", number.constructor.name)         //Number    =>  Es un tipo de dato number.
 
-    console.log("number         : ", number);           //9007199254740992
-    console.log("number + 1:    : ", number + 1);       //9007199254740992
-    console.log("number + 5     : ", number + 5);       //9007199254740996
-    console.log("number + 40    : ", number + 40);      //9007199254741032
+                console.log("number         : ", number);           //9007199254740992
+                console.log("number + 1:    : ", number + 1);       //9007199254740992
+                console.log("number + 40    : ", number + 40);      //9007199254741032
+                console.log("number + 5     : ", number + 5);       //9007199254740996
+                console.log("");
 
-    //2**53 se mantiene dentro de lo límites de MAX_SAFE, por lo tanto si un number exediera ese límite, ocasionaria un perdida de presición.
+                //2**53 da como resultado un Big int, pero al representarse con un number, ocasiona una pérdida de presición.
+
+        //Ejemplo 2 - Big int:
+                const number2 = 2n ** 53n;
+
+                //Averiguamos el tipo de dato.
+                console.log("Tipo de dato de number2: ", number2.constructor.name);
+
+                console.log("number2            : ", number2);
+                console.log("number2 + 1n       : ", number2 + 1n);
+                console.log("number2 + 5n       : ", number2 + 5n);
+                console.log("number2 + 40n      : ", number2 + 40n);
+                console.log("");
+
+                //2n **53n da como resultado un Big int, y al sumarse con un Big int, tenemos un resultado con presición.
     //-------------------------------------------------------------------------------------
     
-    console.log("");
+    console.log("%cMezclando tipo de datos","font-weight: bold;");
+
+    //const number3 = 5 + 5n;             
+    //console.log(number3);             //Cannot mix BigInt and other types, use explicit conversions => NO SE PUEDEN MEZCLAR UN BIG INT Y UN NUMBER.
+
+    const number4 = BigInt(5) + 5n;
+    console.log("number4: ", number4);   //10n => Transforma el 5 a Big int y lo suma con 5n.
+
+    const number5 = 5 + Number(5n);
+    console.log("number5: ", number5); //10 => Transforma 5n a un NUMBER y lo suma con 5.
+
     console.log("");
 
     //-------------------------------------------------------------------------------------
-    console.log("");
+    console.log("Métodos de Big int");
+
+    //BigInt.asIntN(bits, bigNumber)    =>      Devuelve bigNumber(BIG INT) a un NUMBER en  bits (devuelve un entero con signo).
+    //BigInt.asUintN(bits, bigNumber)    =>      Devuelve bigNumber(BIG INT) a un NUMBER en  bits (devuelve unu entero SIN signo).
+    console.log(BigInt.asIntN(2, -3n));
     console.log("");
 
     //-------------------------------------------------------------------------------------
