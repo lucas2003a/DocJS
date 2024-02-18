e5848e8e2294c310c142e90a460dcfa59c3be9d3
lucas2003a <lucasatuncar1@gmail.com>
2024-02-18 14:15:17 -0500

    formas de crear un objeto

---
+++ Test3 (objetos)/js/objetos.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    
+    console.log("%c¿Qué son los objetos?","font-weight: bold; text-transform: uppercase;");
+    console.log("");
+
+    //-----------------------------------------------------------------------------------------
+    console.log("%cFormas de crear un objeto en JavaScript","font-weight: bold;");
+    console.log("");    
+
+    //Forma 1:
+    const objeto = new Object();
+    console.log("typeof objeto  : ", typeof objeto);    //object
+    console.log(`typeof objeto == "object"  : `, typeof objeto == "object");             //true
+    console.log("");
+
+    //Forma 2:
+    const objeto1 = {};
+    console.log("typeof objeto1 : ", typeof objeto1);   //object
+    console.log(`typeof objeto1 == "object"  : `, typeof objeto1 == "object");             //true
+    console.log("");
+
+    console.log("");
+    //-----------------------------------------------------------------------------------------
+    console.log("");
+    console.log("");
+    //-----------------------------------------------------------------------------------------
+    console.log("");
+    console.log("");
+    //-----------------------------------------------------------------------------------------
+    console.log("");
+    console.log("");
+    //-----------------------------------------------------------------------------------------
+    console.log("");
+    console.log("");
+    //-----------------------------------------------------------------------------------------
+    console.log("");
+    console.log("");
+    //-----------------------------------------------------------------------------------------
+    console.log("");
+    console.log("");
+    //-----------------------------------------------------------------------------------------
+    console.log("");
+    console.log("");
+    //-----------------------------------------------------------------------------------------
+    console.log("");
+    console.log("");
+    //-----------------------------------------------------------------------------------------
+    console.log("");
+    console.log("");
+    //-----------------------------------------------------------------------------------------
+});
