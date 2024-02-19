71b09b8f7318f8266797dcc18c4693ecc720d082
lucas2003a <lucasatuncar1@gmail.com>
2024-02-19 00:04:27 -0500

    ¿Qué es un objeto? - toString()

--- Test3 (objetos)/js/objetos.js
+++ Test3 (objetos)/js/objetos.js
@@ -87,18 +87,115 @@ document.addEventListener("DOMContentLoaded",()=>{
     
     //-----------------------------------------------------------------------------------------
     
+    console.log("%cMétodos de un objeto","font-weight: bold;");
     console.log("");
+
+    /*  Hasta ahora solo se ha creado objetos "genéricos", pero si dentro de una varible metemos una función (o una varible que tiene una función), tendríamo lo que se denomina
+        un MÉTODO DE UN OBJETO.
+    */
+
+    const user = {
+        name : "Lucas",
+        talk: function (){return `Hola ${this.name}`;}
+    }
+
+    /*
+        clave : valor
+
+        Para crear una función dentro de un objeto, el nombre será la clave y el valor será la función misma y todo lo que realizará,
+        entonces si queremos llamar a la función dentro de un objeto, necesitamos invocar el nombre de la clave.
+    */
+
+    console.log("user.name      : ", user.name);        // Lucas
+    console.log("user.talk()    : ", user.talk());      // Hola Lucas
+    console.log("");
+
+    const user2 = {
+        name: "Eduardo",
+        age: 20,
+        role: "Admin",
+        exp: 90/100,
+        info:function(){return `Hola ${this.name} esta es tu información:\n\n Edad: ${this.age}\n Rol: ${this.role}\n Experiencia: ${this.exp}`;}
+    }
+    console.log(user2.info());
+    /*
+        Hola Eduardo esta es tu información:
+
+            Edad: 20
+            Rol: Admin
+            Experiencia: 0.9
+    */
+
+    console.log(Math);  //Composición del objeto Math:   Math {abs: ƒ, acos: ƒ, acosh: ƒ, asin: ƒ, asinh: ƒ, …}
     console.log("");
     
     //-----------------------------------------------------------------------------------------
     
+    console.log("%cEl método toString()","font-weight: bold;");
+
+    /*  Simplemente por generara una varible del tipo OBJECT, esta "hereda" una seria de métodos que existe en cualquier varible del tipo OBJECT. 
+        Un buen ejemplo sería el método toString(), un método que intenta representar la información de un objeto en un string.
+    */
+
+    const testObject = {};
+    console.log("testObject.toString()  : ", testObject.toString());        //[object Object]
     console.log("");
+
+    /*  Esto ocurre tabién con otro tipo de datos que no son OBJECT, como NUMBER, BOOLEAN O REGEXP.
+    */
+
+    const number = 42;                                          //Tipo number
+    console.log("number.toString()  : ",number.toString());     //Devuelve 42
+    console.log("");
+
+    const boolean = true;                                       //Tipo boolean
+    console.log("boolean.toString() : ", boolean.toString());   //Devuelve true
+    console.log("");
+
+    const regexp = /.+/;                                        //Tipo RegExp
+    console.log("regexp.toString()  : ", regexp.toString());    //Devuelve "/.+/"
+    console.log("");
+
+
+    /*  Esto debido que al crear una variable de detereminado tipo de dato, la varible será también de tipo OBJECT, ya que todas las varibles heredan este tipo de dato,
+        por lo tanto nuestra varible tendrá:
+
+            *Los métodos que implementaremos nosotros personalmente.
+            *Los métodos heredados de su propio tipo de datos.
+            *Los métodos heredados del tipo OBJECT.
+    */
+
+    const numero = 42.5;
+
+    console.log("numero.toString()          : ",numero.toString());             //Devuelve 42.5 (Método de varibles de tipo OBJECT)
+    console.log("numero.toLocaleString()    : ", numero.toLocaleString());      //Devuelve 42,5 o 42.5 dependiendo del entorno(Método de varibles de tipo OBJECT)
+    console.log("numero.toFixed(3)          : ", numero.toFixed(3));            //Devuelve 42.500 (Método de varibles de tipo Number)
     console.log("");
     
+    /*  toLocaleString() => Cumple con la misma función que toString(), la diferencia es que formatea el valor a ñas configuraciones del entorno local, es decir
+        si en el entorno local estuviese configurado la como como separador de la parte decimal, formatearía el número a 42,5.
+    */
+
     //-----------------------------------------------------------------------------------------
     
+    console.log("%cCreando nuestro toString()","font-weight: bold;");
     console.log("");
-    console.log("");
+
+    const player = {
+        name : "Lucas",
+        life : 4,
+        totalLife : 6,
+        toString : function(){
+            return `${this.name} (${this.life}/${this.totalLife })`;
+        } 
+    }
+    console.log(`Mi jugador es ${player.toString()}`);      //Mi jugador es Lucas (4/6)
+
+    console.log("Mi jugador es " + player);                 //Mi jugador es Lucas (4/6)
+    /*  Como el objeto player no es del tipo STRING, automáticamente llama al método .toString() y obtiene un string para reemplazarlo.
+    */
+
+    console.log("");    
     
     //-----------------------------------------------------------------------------------------
     
